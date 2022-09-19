478a6f6713c9e0139009f673ab299741fb1194d9
LAPTOP-GCDFU2EV\ilya <kiryutkin.02@mail.ru>
2022-09-19 19:50:10 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Home());
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+        title: Text("Приложение"),
+        centerTitle: true,
+      )),
+    );
+  }
+}
